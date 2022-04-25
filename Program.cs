e0d718a33db10b4f26b08719b5a3cb320821fa78
Thiago <Thiagolopespereiralp1@gmail.com>
2022-04-25 18:15:03 -0300

    finalização do exercicio

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿Double altura, pesoideal;
+String sexo;
+
+Console.Write("Digite sua altura em m..........: ");
+altura = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Sexo [M]asculino / [F]eminino...: ");
+sexo = Console.ReadLine()!.ToUpper().Trim();
+
+if (sexo != "M" && sexo != "F")
+{
+    Console.WriteLine("Não calculavel");
+}
+
+else 
+{
+     if (sexo == "M")
+     {
+         pesoideal = altura * 72.7 - 58.0;
+     }
+     else 
+     {
+         pesoideal = altura * 62.1 - 44.7;
+     }
+
+     Console.WriteLine($"Seu peso ideal é {pesoideal:N1}kg.");
+}
